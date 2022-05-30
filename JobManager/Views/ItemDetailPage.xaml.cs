79243a411371868ec2b976d9cafd2b7fe2a153c6
savithamk <savitha.maiya1@gmail.com>
2022-05-30 18:32:47 -0400

    Initial commit

---
+++ JobManager/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using JobManager.ViewModels;
+
+namespace JobManager.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
