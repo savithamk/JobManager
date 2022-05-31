b422f7e3e71cfefbce8265c3cddbc21677a27d88
savithamk <savitha.maiya1@gmail.com>
2022-05-31 11:39:59 -0400

    Added a model class for Job and an interface to expose services.
    Created a Job class in models with multiple attributes and made this class as public so that, this class can be accessed in other classes especially in services.
    Job class contains three attributes id, name and description with setters and getters to read and manipulate data.
    
    Created a IJobDataStore interface under services to expose services to read or write job data.
    This interface has methods for the following CRUD operations.
    1.	Created GetJobs() to fetch all the jobs.
    2.	Created GetJob() by passing id to fetch a particular job.
    3.	Created AddJob() to insert new job to the database
    4.	Created UpdateJob() to update a specific job in the table.

---
+++ JobManager/Models/Job.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace JobManager.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ JobManager/Services/IJobDataStore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using JobManager.Models;
+
+namespace JobManager.Services
+{
+    public interface IJobDataStore<T>
+    {
+        Task<IEnumerable<Job>> GetJobs();
+
+        Task<Job> GetJob(int id);
+
+        Task AddJob(Job job);
+
+        Task UpdateJob(Job job);
+
+    }
+}
