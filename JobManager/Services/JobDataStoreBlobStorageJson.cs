b577d306a5566d76ddfdf2447e4cb7c6ac0206b5
savithamk <savitha.maiya1@gmail.com>
2022-06-14 23:01:35 -0400

    Added azure storage blobs dependency to integrate with Azure storage account for Job data storage.
    Created a resource in azure and accessed key and connection string from azure storage account to use in the application
    
    Created a new implementation (JobDataStoreBlobStorageJson) of IJobDataStore interface to integrate with azure storage. Added the connection string acquired from storage account, defined the container name and file name.
    
    Created a service client of type BlobServiceClient by passing the connection string to interact with azure storage services.
    
    Implemented WriteFile method to write the list of jobs to the  azure store using factory methods from BlobServiceClient.
    
    Added the default methods for the rest of the methods of the interface.

---
+++ JobManager/Services/JobDataStoreBlobStorageJson.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using JobManager.Models;
+using Newtonsoft.Json;
+
+namespace JobManager.Services
+{
+    class JobDataStoreBlobStorageJson : IJobDataStore<Job>
+    {
+
+        private static string ConnectionString => "DefaultEndpointsProtocol=https;AccountName=052022jobmanager;AccountKey=lRAoAesneyu2nrer0QCBC+GnOUhAtxYTqMj+tClvHRYmZIDTzTgSxqIEoFL3TjzS7wctPdLMM0oO+AStTGnmWA==;EndpointSuffix=core.windows.net";
+
+        private static string Container => "data";
+
+        private static string FileName => "Jobs.json";
+
+        private readonly BlobServiceClient serviceClient = new BlobServiceClient(ConnectionString);
+
+        public async Task WriteFile(List<Job> jobs)
+        {
+            var jsonString = JsonConvert.SerializeObject(jobs);
+
+            var stream = new MemoryStream();
+
+            var writer = new StreamWriter(stream);
+            writer.Write(jsonString);
+
+            writer.Flush();
+
+            stream.Position = 0;
+            BlobClient blob = serviceClient.GetBlobContainerClient(Container).GetBlobClient(FileName);
+
+            await blob.UploadAsync(stream);
+
+        }
+
+        public async Task AddJob(Job job)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteJob(Job job)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Job> GetJob(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Job>> GetJobs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateJob(Job job)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
